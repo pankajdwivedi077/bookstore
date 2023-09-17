cc10f48929b2daa84486d237b16a45ed60334d34
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2023-09-17 13:20:46 +0530

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,11 @@ import axios from 'axios';
 import Spinner from '../components/Spinner';
 import { Link } from 'react-router-dom';
 import { MdOutlineAddBox } from 'react-icons/md';
-import BooksTable from '../components/home/BooksTable';
-import BooksCard from '../components/home/BooksCard';
+import BooksCard from '../components/home/BooksCard'; // Import BooksCard (assuming it's the default export)
 
 const Home = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [showType, setShowType] = useState('table');
 
   useEffect(() => {
     setLoading(true);
@@ -28,18 +26,19 @@ const Home = () => {
   return (
     <div className='p-4'>
       <div className='flex justify-center items-center gap-x-4'>
-        <button
-          className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
+        {/* Buttons for switching between table and card view */}
+        {/* <button
+          className='bg-sky-300 hover.bg-sky-600 px-4 py-1 rounded-lg'
           onClick={() => setShowType('table')}
         >
           Table
         </button>
         <button
-          className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
+          className='bg-sky-300 hover.bg-sky-600 px-4 py-1 rounded-lg'
           onClick={() => setShowType('card')}
         >
           Card
-        </button>
+        </button> */}
       </div>
       <div className='flex justify-between items-center'>
         <h1 className='text-3xl my-8'>Books List</h1>
@@ -49,10 +48,8 @@ const Home = () => {
       </div>
       {loading ? (
         <Spinner />
-      ) : showType === 'table' ? (
-        <BooksTable books={books} />
       ) : (
-        <BooksCard books={books} />
+        <BooksCard books={books} /> // Use the BooksCard component
       )}
     </div>
   );
